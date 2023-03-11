cc25b483b501a1d7e9e189223211196fbce74120
krisbobo <kiyongbohbo@gmail.com>
2023-03-11 23:31:31 +0100

    Implement changes

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,31 +1,25 @@
+/* eslint-disable */
+import { createSlice } from '@reduxjs/toolkit';
 import books from '../state/stateSlice';
-
-const ADD_BOOK = 'bookstore/books/ADD_BOOK';
-const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
 
 const initialState = { books };
 
-const remove = (state, bookID) => state.filter((book) => book.item_id !== bookID);
-
-export default (state = initialState, action = {}) => {
-  switch (action.type) {
-    case ADD_BOOK:
-      return [
-        ...state,
-        action.payload,
-      ];
-    case REMOVE_BOOK:
-      return remove(state, action.item_id);
-    default: return state;
-  }
-};
-
-export const addBook = (payload) => ({
-  type: ADD_BOOK,
-  payload,
+const booksSlice = createSlice({
+  name: 'books',
+  initialState,
+  reducers: {
+    addBook: (state, action) => {
+      state.books = [...state.books, action.payload];
+      console.log(state.books)
+    },
+    removeBook: (state, { payload }) => ({
+      ...state,
+      books: state.books.filter((book) => book.item_id !== payload),
+    }),
+  },
 });
 
-export const removeBook = (id) => ({
-  type: REMOVE_BOOK,
-  id,
-});
+export const { addBook, removeBook } = booksSlice.actions;
+
+export default booksSlice.reducer;
+
