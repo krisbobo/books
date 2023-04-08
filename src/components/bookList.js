8df36003165a3703e50fc819014d5be2c7a15e28
krisbobo <kiyongbohbo@gmail.com>
2023-04-08 12:09:16 +0100

    Refactor: update file to implement features

--- src/components/bookList.js
+++ src/components/bookList.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import DisplayBooks from './displaybooks';
+import { getBooks } from '../redux/books/books';
 
 const BookList = () => {
-  const { books } = useSelector((state) => state.books);
-  const bookData = books;
+  const dispatch = useDispatch();
+  const books = useSelector((state) => state.books.books);
+  useEffect(() => {
+    dispatch(getBooks());
+  }, [dispatch]);
 
   return (
     <>
       <div className="container">
-        {bookData.map((book) => <DisplayBooks key={bookData.item_id} book={book} />)}
+        {books && books.map((book) => <DisplayBooks key={book.id} book={book} />)}
       </div>
       <div id="line" />
     </>
